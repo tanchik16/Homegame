187b39a81f0ab287cf06bfbb27eea8baa58725e5
tanchik16 <dtv79@mail.ru>
2022-12-26 21:28:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def greet():
+    print("  Приветсвуем вас  ")
+    print("      в игре       ")
+    print("  крестики-нолики  ")
+    print("-------------------")
+    print(" формат ввода: x y ")
+    print(" x - номер строки  ")
+    print(" y - номер столбца ")
+
+greet()
+field = [[" "] * 3 for i in range(3)]
+
+
+def show():
+    print(f" 0| 1| 2|")
+    for i in range(3):
+        row_info = " ".join(field[i])
+        print("\t")
+        print(f"{i}{row_info}")
+
+show()
+def ask():
+    while True:
+        datas = input("         Ваш ход: ").split()
+
+        if len(datas) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+
+        x, y = datas
+
+        if not (x.isdigit()) or not (y.isdigit()):
+            print(" Введите числа! ")
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Координаты вне диапазона! ")
+            continue
+
+        if field[x][y] != " ":
+            print(" Клетка занята! ")
+            continue
+
+        return x, y
+ask()
+def check_win():
+    win_data = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for data in win_data:
+        symbols = []
+        for d in data:
+            symbols.append(field[d[0]][d[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X!!!")
+            return True
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0!!!")
+            return True
+    return False
+count = 0
+while True:
+    count += 1
+    show()
+    if count % 2 == 1:
+        print(" Ходит крестик!")
+    else:
+        print(" Ходит нолик!")
+
+    x, y = ask()
+
+    if count % 2 == 1:
+        field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+
+    if check_win():
+        break
+
+    if count == 9:
+        print(" Ничья!")
+        break
